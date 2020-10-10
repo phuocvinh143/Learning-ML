f29d38decc0287a7ee82ce58af035d78b8b73ff3
phuocvinh143 <maiphuocvinh143@gmail.com>
2020-10-10 18:56:14 +0700

    no message

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -1,6 +1,8 @@
 from sklearn import linear_model
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 
 def find_by_equation(X, y):
@@ -42,16 +44,37 @@ def find_by_module(X, y):
     regr = linear_model.LinearRegression(fit_intercept=False)  # fit_intercept = False for calculating the bias
     regr.fit(Xbar, y)
 
+    return regr.coef_
+
+
+def predict_weight():
+    # height (cm)
+    X = np.array([[147, 150, 153, 155, 158, 160, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
+
+    # weight (kq)
+    y = np.array([[55, 56, 57, 59, 60, 61, 63, 64, 65, 67, 68, 69, 71, 72, 73]]).T
+    find_by_equation(X, y)
+
+    w = find_by_module(X, y)
+
     height = [162, 169]
-    weight_predict = [regr.coef_[0][0] + regr.coef_[0][1] * x for x in height]
+    weight_predict = [w[0][0] + w[0][1] * x for x in height]
     print(weight_predict)
 
 
-# height (cm)
-X = np.array([[147, 150, 153, 155, 158, 160, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
+def acc_cal(x, y):
+    return np.sum(x == y)
 
-# weight (kq)
-y = np.array([[55, 56, 57, 59, 60, 61, 63, 64, 65, 67, 68, 69, 71, 72, 73]]).T
 
-find_by_equation(X, y)
-find_by_module(X, y)
+def predict_iris_flowers():
+    iris = datasets.load_iris()
+    X = iris['data']
+    y = iris['target']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=50)
+    w = find_by_module(X_train, y_train)
+    y_predict = [int(abs(round(np.dot(x, w[1:]), 0))) for x in X]
+
+    print(acc_cal(y_predict, y)/150)
+
+predict_iris_flowers()
